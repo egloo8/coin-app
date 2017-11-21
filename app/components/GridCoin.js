fe1f91255965bdc71fad7e2f15f51b07ce1d384d
egloo8 <egle.lakyte@gmail.com>
2017-11-21 21:51:49 +0000

    Opacity does not change just after you change the amount of Coins. Did not find a good solution, will look into Redux

--- app/components/GridCoin.js
+++ app/components/GridCoin.js
@@ -3,36 +3,68 @@ import { View, StyleSheet, Image, Dimensions, TouchableHighlight, AsyncStorage, 
 import Divider from './Divider'
 
 class GridCoin extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     state = {
         opacity: 0,
         value: ''
     }
 
+    componentDidMount() {
+        this.getOpacity()
+    }
+
     getOpacity = () => {
         AsyncStorage.getItem(this.props.coin.id + '/amount').then(value => {
-            this.setState({
-                value: value
-            })
+            if (this.state.value !== value) {
+                this.setState({
+                    value: value
+                })
+                if (value && value !== '0') {
+                    this.setState({
+                        opacity: 1
+                    })
+                } else {
+                    this.setState({
+                        opacity: 0.5
+                    })
+                }
+            }
         })
     }
 
+    // async getOpacity() {
+    //     var opacity
+    //     const value = await AsyncStorage.getItem(this.props.coin.id + '/amount')
+    //     if (value && value !== '0') {
+    //         opacity = 1
+    //     } else {
+    //         opacity = 0.5
+    //     }
+    //     return opacity
+    // }
 
     render() {
         const coin = this.props.coin
-        this.getOpacity()
-        let opacity = 0
-        if (this.state.value && this.state.value !== '0') {
-            opacity = 1
-        } else {
-            opacity = 0.5
-        }
+        // var opacity
+        // this.getOpacity().then((opacity) => {
+        //     if (value && value !== '0') {
+        //         opacity = 1
+        //     } else {
+        //         opacity = 0
+        //     }
+        //     console.log(opacity)
+        // })
+
         return (
             <View
                 style={styles.box}
                 key={coin.id}
             >
                 <TouchableWithoutFeedback onPress={() => this.props.onCoinPress(coin)} disabled={this.props.disabled}>
-                    <Image source={coin.src} style={{ width: '100%', height: '100%', opacity: opacity }} />
+                    <Image source={coin.src} style={{ width: '100%', height: '100%', opacity: this.state.opacity }} />
                 </TouchableWithoutFeedback>
             </View>
         )
