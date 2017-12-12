783ce18bc8d2c043152980dd01ee9e4a7343f1cb
egloo8 <egle.lakyte@gmail.com>
2017-12-12 20:10:34 +0000

    New try with redux

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+
+import store from './src/Store'
+import Router from './src/config/Router'
+import GridContainer from './src/containers/GridContainer'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider >
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
