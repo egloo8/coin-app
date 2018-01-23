2870b90f13762ffee0ea134d5d44f122ab41e5d1
egloo8 <egle.lakyte@gmail.com>
2018-01-23 12:18:12 +0000

    Removed my bad MongoStorage try

--- src/Store.js
+++ src/Store.js
@@ -6,32 +6,12 @@ import logger from 'redux-logger'
 import { autoRehydrate, persistStore } from 'redux-persist'
 import RootReducer from './reducers'
 
-import { loadState } from './config/MongoStorage'
-
-// const size = 20
-// let coins = []
-
-// for (let i = 0; i < size; i++) {
-//     coins.push({
-//         id: i,
-//         amount: 0
-//     })
-// }
-
-// const initialState = { coins }
-
-// // const persistedState = loadState()
-// loadState().then((preloadedState) => {
-//     console.log(preloadedState)
-// })
-
 let store = compose(
     autoRehydrate(),
     applyMiddleware(thunk, logger)
 )(createStore)(RootReducer)
 
 // AsyncStorage.clear()
-// console.log(AsyncStorage.getAllKeys())
 
 persistStore(store, { storage: AsyncStorage })
 
